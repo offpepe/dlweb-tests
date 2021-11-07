5304c899ea838b1ff93b7c06b348f54a886fdd8e
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-11-07 14:03:07 -0400

    feat: custom document config

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+const Document = () => (
+<>
+    <Html>
+        <Head>
+        <meta name="description" content="LandingPage to dlweb pratical test" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </Html>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+</>
+)
+
+export default Document;
