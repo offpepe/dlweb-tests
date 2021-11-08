bc10aacf219e9fc7d5df700f6f3e76d905045281
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-11-08 18:56:31 -0400

    feat: prices Component

---
+++ components/Prices.jsx
@@ -0,0 +1,23 @@
+import styles from '../styles/Prices.module.css';
+
+export default function Prices() {
+    return (
+        <section className={ styles.Prices }>
+            <span className={ styles.PricesTop } />
+            <div style={ { padding: '128px', height: '542px' } }>
+            <div  className={ styles.PricesContent }>
+                <h2>
+                  Conheça os nossos preços
+                </h2>
+                <p>
+                  Adquira o carregamento VIP e faça sorteios de forma rápida e segura!<br/>
+                  E se você tem mais de 300 mil seguidores, aproveite as vantagens<br/>
+                  exclusivas sorteio.com para influenciadores digitais. 
+                </p>
+            </div>
+            <button> Quero conhecer </button>
+            </div>
+            <span className={ styles.PricesBottom } />
+        </section>
+    )
+}
