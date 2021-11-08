0ce64d6ee9cd8ef7c9f57219a27c7c9a74743c11
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-11-08 14:01:01 -0400

    feat: loadingDescription section

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import HeaderMenu from '../components/HeaderMenu';
 import Header from '../components/Header';
 import WhatsNew from '../components/WhatsNew';
+import LoadingDescription from '../components/LoadingDescription';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
     <HeaderMenu />
     <Header />
     <WhatsNew />
+    <LoadingDescription />
   </>
   )
 }
