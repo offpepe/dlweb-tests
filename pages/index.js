42a728379f60c4b514bd9d1e39eb40aeee20f311
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-11-08 11:01:20 -0400

    feat: whatsNew section

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import Head from 'next/head';
 import HeaderMenu from '../components/HeaderMenu';
 import Header from '../components/Header';
+import WhatsNew from '../components/WhatsNew';
 
 export default function Home() {
   return (
   <>
-   <Head>
-     <title> sorteio.com </title>
-   </Head>
-   <HeaderMenu />
-     <Header />
+    <Head>
+      <title> sorteio.com </title>
+    </Head>
+    <HeaderMenu />
+    <Header />
+    <WhatsNew />
   </>
   )
 }
