314a395f26a88d352eb7b889e40dc0a0931189e6
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-11-08 10:27:35 -0400

    feat: header component in LP

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import HeaderMenu from '../components/HeaderMenu';
+import Header from '../components/Header';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
      <title> sorteio.com </title>
    </Head>
    <HeaderMenu />
+     <Header />
   </>
   )
 }
