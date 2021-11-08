c7096cf61d1e82899942920c2a9d8eca5c21795a
Alan Albuquerque Ferreira Lopes <alan.alb.flopes@gmail.com>
2021-11-08 18:56:20 -0400

    feat: prices section

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Header from '../components/Header';
 import WhatsNew from '../components/WhatsNew';
 import LoadingDescription from '../components/LoadingDescription';
 import WhoUses from '../components/WhoUses';
+import Prices from '../components/Prices';
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
     <WhatsNew />
     <LoadingDescription />
     <WhoUses />
+    <Prices />
   </>
   )
 }
